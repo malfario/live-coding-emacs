d4fbe4d973334b17ee61b92fd0fb40bddfd3cd95
Rafa Leblic <rafa@notin.net>
2011-11-21 18:03:22 +0100

    Add emacs 24 color themes support

--- config/bindings.el
+++ config/bindings.el
@@ -1,1 +1,11 @@
 ;; Place personal bindings here
+
+;; Linenum margin
+(setq linum-format "%d ")
+
+;; Goto line alias
+(global-set-key (kbd "C-x C-g") 'goto-line)
+
+;; Default font
+(set-default-font 
+ "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
--- config/core.el
+++ config/core.el
@@ -3,8 +3,16 @@
 ;; such as this colour theme (feel free to change it to your own favourite theme)
 
 ;; use blackbored colour theme
-(load-file (concat dotfiles-lib-dir "blackbored.el"))
-(color-theme-blackbored)
+;(load-file (concat dotfiles-lib-dir "blackbored.el"))
+;(color-theme-blackbored)
+
+(setq dotfiles-theme-dir (concat dotfiles-dir "themes/"))
+
+(defun add-dotfile-theme (p)
+  (add-to-list 'custom-theme-load-path (concat dotfiles-theme-dir p)))
+
+(add-dotfile-theme "zenburn-emacs/")
+(load-theme 'zenburn)
 
 ;;Or load external files such as this bindings file:
 (load-dotfile "config/bindings.el")
